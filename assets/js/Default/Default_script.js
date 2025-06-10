e51b90d10eb2cfe703d4e3ae86bab2d224284318
Rod Dominic <dam083105@gmail.com>
2025-06-11 01:00:40 +0800

    ver 1.2 | about download file

--- assets/js/Default/Default_script.js
+++ assets/js/Default/Default_script.js
@@ -1,15 +1,19 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const backToTopBtn = document.getElementById('back-to-top');
-  
+
     if (scrollPosition > 100) {
         backToTopBtn.style.opacity = 1;
     } else {
         backToTopBtn.style.opacity = 0;
     }
-  });
+});
   
-  document.getElementById('back-to-top').addEventListener('click', function(e) {
+document.getElementById('back-to-top').addEventListener('click', function(e) {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
+});
+
+function btnDownload() {
+    window.print();
+}
