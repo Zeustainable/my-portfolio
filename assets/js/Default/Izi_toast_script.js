3152ba61b5f040a7e2e017a5e16c6e6602e316e7
Rod Dominic <dam083105@gmail.com>
2025-07-06 15:32:36 +0800

    ver 2.6 | implemented a modal to all projects

--- assets/js/Default/Izi_toast_script.js
+++ assets/js/Default/Izi_toast_script.js
@@ -17,23 +17,23 @@ function showToast(type, title, message) {
 //SAMPLE RUN: showConfirmationToast('Display!', 'Are you sure you want to display?')
 
 function showConfirmationToast(title, message) {
-  iziToast.question({
-    timeout: false,
-    close: false,
-    overlay: true,
-    displayMode: 'once',
-    title: title,
-    message: message,
-    position: 'topCenter',
-    buttons: [
-      ['<button><b>YES</b></button>', function (instance, toast) {
-        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-        showToast('success', 'Success!', 'Your changes have been applied.');
-      }, true],
-      
-      ['<button>Cancel</button>', function (instance, toast) {
-        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-      }]
-    ]
-  });
+    iziToast.question({
+        timeout: false,
+        close: false,
+        overlay: true,
+        displayMode: 'once',
+        title: title,
+        message: message,
+        position: 'topCenter',
+        buttons: [
+            ['<button><b>YES</b></button>', function (instance, toast) {
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                showToast('success', 'Success!', 'Your changes have been applied.');
+            }, true],
+
+            ['<button>Cancel</button>', function (instance, toast) {
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+            }]
+        ]
+    });
 }
